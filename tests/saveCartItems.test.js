9695fb15f46d4ab7ac404eff693506f00460a5c4
Erika de Oliveira Lima <erikadeo.lima@hotmail.com>
2022-04-29 19:08:37 -0300

    resquisito 10

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,12 @@ localStorageSimulator('setItem');
 
 describe('4 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Teste se, ao executar `saveCartItems` com o argumento `<ol><li>Item</li></ol>`, o método `localStorage.setItem` é chamado;', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);/* testa quantas vezes meu local storage é chamado */
+  });
+  it('Teste se, ao executar `saveCartItems` com o argumento `<ol><li>Item</li></ol>`, o método `localStorage.setItem` é chamado com dois parâmetros, sendo o primeiro `cartItems` e o segundo sendo o valor passado como argumento para `saveCartItems`.', () => {
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems', '<ol><li>Item</li></ol>');/* testa se eu local storage chama o pai (cartItems) e o filho (li)*/
+  });
 });
